eec87e56243fea67e61405ef829be63487b178de
Mikołaj Szychowiak <mikolaj.szychowiak@gmail.com>
2015-03-16 23:52:38 +0100

    Konto implemented

--- Bank/src/pl/put/miasi/bank/Konto.java
+++ Bank/src/pl/put/miasi/bank/Konto.java
@@ -1,5 +1,13 @@
 package pl.put.miasi.bank;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 
  * @author Mikołaj Szychowiak
@@ -7,12 +15,82 @@ package pl.put.miasi.bank;
  */
 public class Konto {
 
+	protected static class Wpis {
+		private final Operacja operacja;
+		private final List<Object> params;		
+		public Wpis(Operacja operacja, Object... params) {
+			this.operacja = operacja;
+			this.params = new ArrayList<Object>(Arrays.asList(params));
+		}
+		public Operacja getOperacja() {
+			return operacja;
+		}
+		public List<Object> getParams() {
+			return params;
+		}
+		
+	}
+	
 	private double saldo;
 	
 	private final Wlasciciel wlasciciel;
 	
+	private final Map<Date, Wpis> historia;//co zamiast inta?
+
+	private double debet;
+	
 	public Konto(Wlasciciel wlasciciel) {
-		this.wlasciciel = wlasciciel;
+		this(wlasciciel, 0);
+	}
+	
+	public Konto(Wlasciciel wlasciciel, double saldo) {
+		this.wlasciciel = wlasciciel;		
+		this.saldo = saldo;
+		historia = new HashMap<Date, Wpis>();		
+	}	
+	
+	public void wplata(double kwota) {
+		if (kwota >=0) {
+			historia.put(new Date(System.currentTimeMillis()), new Wpis(Operacja.WPLATA, kwota));
+			saldo += kwota;
+		} else {
+			throw new IllegalArgumentException("Wpłata nie może być ujemna");
+		}
+	}
+	
+	public void wyplata(double kwota) {
+		if (kwota >=0 && saldo - kwota >= -debet) {
+			historia.put(new Date(System.currentTimeMillis()), new Wpis(Operacja.WYPLATA, kwota));
+			saldo += kwota;
+		} else if (kwota >=0) {
+			throw new IllegalStateException("Brak wystarczających środków na koncie");
+		} else {
+			throw new IllegalArgumentException("Wypłata nie może być ujemna");
+		}
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Wlasciciel getWlasciciel() {
+		return wlasciciel;
+	}
+
+	public Map<Date, Wpis> getHistoria() {
+		return historia;
+	}
+
+	public double getDebet() {
+		return debet;
+	}
+
+	public void setDebet(double debet) {
+		this.debet = debet;
 	}
 	
 }
---
+++ Bank/src/pl/put/miasi/bank/Operacja.java
@@ -0,0 +1,5 @@
+package pl.put.miasi.bank;
+
+public enum Operacja {
+	WPLATA, WYPLATA, PRZELEW;
+}
