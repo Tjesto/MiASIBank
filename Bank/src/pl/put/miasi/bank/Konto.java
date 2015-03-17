3c23b8f2ab812fcd8bc52c817d903726b1296896
User Two <UserTwo@domain.com>
2015-03-17 11:42:10 +0100

    Tests added

--- Bank/src/pl/put/miasi/bank/Konto.java
+++ Bank/src/pl/put/miasi/bank/Konto.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 /**
  * 
- * @author Mikołaj Szychowiak
+ * @author Mikolaj Szychowiak
  *
  */
 public class Konto {
---
+++ Bank/tests/pl/put/miasi/bank/KontoTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package pl.put.miasi.bank;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author inf106580
+ *
+ */
+public class KontoTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link pl.put.miasi.bank.Konto#Konto(pl.put.miasi.bank.Wlasciciel)}.
+	 */
+	@Test
+	public void testKontoWlasciciel() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.put.miasi.bank.Konto#Konto(pl.put.miasi.bank.Wlasciciel, double)}.
+	 */
+	@Test
+	public void testKontoWlascicielDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.put.miasi.bank.Konto#wplata(double)}.
+	 */
+	@Test
+	public void testWplata() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link pl.put.miasi.bank.Konto#wyplata(double)}.
+	 */
+	@Test
+	public void testWyplata() {
+		fail("Not yet implemented");
+	}
+
+}
